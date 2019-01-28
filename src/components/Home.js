77e0028c091ef3aaaef2e6cc0214a2b2be828f93
Tuan Pham <tuanvpham32@outlook.com>
2019-01-27 23:02:31 -0500

    fix websocket

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,30 +4,45 @@ import RetroActionItemForm from "./RetroActionItemsForm";
 
 
 export default class Home extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             sessionName: "Test",
             actionItems: []
         }
+        this.socket = new WebSocket(
+            "ws://localhost:8000/retro/" + this.state.sessionName + "/?" + props.email
+        )
+
+        this.socket.onmessage = function (e) {
+            console.log(e.data)
+            // this.setState({actionItems: [...this.state.actionItems, e.data]});
+        }
+        this.submitText = this.submitText.bind(this)
+    }
+
+    submitText(e, data) {
+        e.preventDefault()
+        console.log(data)
+        this.socket.send(JSON.stringify(data))
     }
 
 
     render() {
-        const socket = new WebSocket(
-            "ws://localhost:8000/retro/" + this.state.sessionName + "/?" + this.props.email
-        )
+        // const socket = new WebSocket(
+        //     "ws://localhost:8000/retro/" + this.state.sessionName + "/?" + this.props.email
+        // )
         
-        function submitText(e, data) {
-            e.preventDefault()
-            console.log(data)
-            socket.send(JSON.stringify(data))
-        }
+        // function submitText(e, data) {
+        //     e.preventDefault()
+        //     console.log(data)
+        //     socket.send(JSON.stringify(data))
+        // }
 
-        socket.onmessage = function (e) {
-            console.log(e.data)
-            // this.setState({actionItems: [...this.state.actionItems, e.data]});
-        }
+        // socket.onmessage = function (e) {
+        //     console.log(e.data)
+        //     // this.setState({actionItems: [...this.state.actionItems, e.data]});
+        // }
 
         return (
             <div>
@@ -35,7 +50,7 @@ export default class Home extends Component {
                 <button onClick={this.props.handle_logout}>Logout</button>
                 <h2> Team name - {this.state.sessionName}</h2>
                 <RetroActionItems messages={this.state.actionItems} />
-                <RetroActionItemForm submitItem={submitText}/>
+                <RetroActionItemForm submitItem={this.submitText}/>
             </div>
         );
     }
