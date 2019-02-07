b87805cb0f5ea3b0bdb49b3357fc3e24f75cf74d
Tuan Pham <tuanvpham32@outlook.com>
2019-02-06 20:51:37 -0500

    add minor stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,23 +13,47 @@ export default class Home extends Component {
         this.socket = new WebSocket(
             "ws://localhost:8000/retro/" + this.state.sessionName + "/?" + props.email
         )
-
-        console.log("this is session name: " + this.state.sessionName)
-        console.log("this is prop email: " + props.email)
         this.submitText = this.submitText.bind(this)
     }
 
     componentDidMount() {
+        fetch('http://localhost:8000/retro-action-items/', {
+            headers: {
+                Authorization: `JWT ${localStorage.getItem('token')}`
+            }
+        })
+        .then(res => res.json())
+        .then(json => {
+            if (json.detail === "Signature has expired.") {
+                this.setState({
+                    logged_in: false
+                })
+            } else {
+                this.setState({
+                   actionItems: [...this.state.actionItems, ...json]
+                });
+            }
+        });
+
         this.socket.onmessage = function (e) {
-            console.log(e.data)
-            // this.setState({ actionItems: [...this.state.actionItems, e.data] });
+            console.log("this is onmessage: " + e.data)
+            this.addRetroActionItems(e.data)
+        }
+
+        this.socket.onclose = () => {
+            console.log('disconnected')
         }
     }
     submitText(e, data) {
         e.preventDefault()
-        console.log(data)
+        console.log("this is submitText: " + JSON.stringify(data))
         this.socket.send(JSON.stringify(data))
+        this.addRetroActionItems(data.actionItemText)
     }
+
+    addRetroActionItems = item => this.setState(state => ({
+        actionItems: [item, ...state.actionItems]
+    }))
 
 
     render() {
@@ -38,7 +62,7 @@ export default class Home extends Component {
                 <h1>Hey Dude, this is Home Component. Welcome, {this.props.username} </h1>
                 <button onClick={this.props.handle_logout}>Logout</button>
                 <h2> Team name - {this.state.sessionName}</h2>
-                <RetroActionItems messages={this.state.actionItems} />
+                {/* <RetroActionItems messages={this.state.actionItems} /> */}
                 <RetroActionItemForm submitItem={this.submitText}/>
             </div>
         );
