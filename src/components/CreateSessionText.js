9a55872290c82999d6912961f1f816bba09583df
DrElyk <doctorelyk@gmail.com>
2019-04-07 23:32:45 -0400

    allow session creation with new fields

--- src/components/CreateSessionText.js
+++ src/components/CreateSessionText.js
@@ -5,9 +5,13 @@ export default class PokerEditPoints extends Component {
         super(props);
         this.handleTextChange = this.handleTextChange.bind(this);
         this.handleSelectChange = this.handleSelectChange.bind(this);
+        this.handleCardChange = this.handleCardChange.bind(this);
+        this.handleVelocityChange = this.handleVelocityChange.bind(this);
         this.state={
             entered_text: "",
-            selected_type: "poker"
+            selected_type: "poker",
+            card_type: "Fibonacci",
+            velocity: "50"
         }
     }
 
@@ -22,6 +26,17 @@ export default class PokerEditPoints extends Component {
             selected_type: event.target.value
         });
     }
+    handleCardChange(event) {
+        this.setState({
+            card_type: event.target.value
+        });
+    }
+    handleVelocityChange(event) {
+        let newVel = event.target.value;
+        this.setState({
+            velocity: newVel
+        });
+    }
 
     toHyphens() {
         this.setState({
@@ -31,14 +46,35 @@ export default class PokerEditPoints extends Component {
 
     render() {
         return (
-            <form onSubmit={e => this.props.createSession(e, this.state.entered_text, this.state.selected_type)}>
-                <h4><input type="text" value={this.state.entered_text} onChange={this.handleTextChange} /></h4>
-                <select onChange={this.handleSelectChange}>
-                    <option value="poker">Planning Poker</option>
-                    <option value="retro">Retrospective Board</option>
-                </select>
-                <input type="submit" value="Save Session" onClick={() => this.toHyphens()}/>
-            </form>
+            <div>
+                {this.state.selected_type === "poker" ?
+                    <form onSubmit={e => this.props.createSessionPoker(e, this.state.entered_text, this.state.selected_type, this.state.card_type, this.state.velocity)}>
+                        <h4><input type="text" value={this.state.entered_text} onChange={this.handleTextChange} /></h4>
+                        <select onChange={this.handleSelectChange}>
+                            <option value="poker">Planning Poker</option>
+                            <option value="retro">Retrospective Board</option>
+                        </select>
+                        <div>
+                            <h4>Card Type:</h4>
+                            <select onChange={this.handleCardChange}>
+                                <option value="fib">Fibonacci</option>
+                                <option value="modfib">Modified Fibonacci</option>
+                                <option value="seq">Sequential</option>
+                            </select>
+                            <h4><input type="number" value={this.state.velocity} onChange={this.handleVelocityChange} /></h4>
+                        </div>
+                        <input type="submit" value="Save Session" onClick={() => this.toHyphens()}/> 
+                    </form> :
+                    <form onSubmit={e => this.props.createSession(e, this.state.entered_text, this.state.selected_type)}>
+                        <h4><input type="text" value={this.state.entered_text} onChange={this.handleTextChange} /></h4>
+                        <select onChange={this.handleSelectChange}>
+                            <option value="poker">Planning Poker</option>
+                            <option value="retro">Retrospective Board</option>
+                        </select>
+                        <input type="submit" value="Save Session" onClick={() => this.toHyphens()}/>
+                    </form>
+                }
+            </div>
         );
     }
 }
